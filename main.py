c1cdde8d08d0b125d49bd6ca5e37d7d7f66b33a6
Matej Oblak <74320191+MatejGitOblak@users.noreply.github.com>
2022-11-21 20:13:49 +0100

    added the map of slovenija

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+import matplotlib.pyplot as plt
 import plotly.express as px
 import pandas as pd
 import geopandas as gpd
+import json
+import numpy as np
+import geojson
 
+map_df = gpd.read_file('slovenija_map/obcine/obcine.shp')
+map_df = map_df.to_crs("WGS84")
+map_df = map_df.rename({'NAZIV': 'District'}, axis = 'columns')
+map_df = map_df.drop(columns = ['EID_OBCINA',  'SIFRA', 'NAZIV_DJ', 'OZNAKA_MES', 'DATUM_SYS'])
+map_df["rand"] = np.random.randint(1, 100, len(map_df))
+map_df["geometry"] = (
+    map_df.to_crs(map_df.estimate_utm_crs()).simplify(100).to_crs(map_df.crs)
+)
+map_df.to_file('slovenija_map/obcine/obcine.json', driver="GeoJSON") 
+with open('slovenija_map/obcine/obcine.json', encoding="UTF-8") as f:
+    data = json.load(f)
 
+fig = px.choropleth(map_df,
+                    geojson=data, 
+                    color="rand", 
+                    locations="District", 
+                    featureidkey="properties.District"
+                   )
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 app = Dash(__name__)
 
-app.layout = html.Div(children=[
+app.layout = html.Div(className="main-div", children=[
+    html.Div(className="div1", children=[
+        html.Div("div1")
+    ]),
+    html.Div(className="div2", children=[
+        html.Div("div2")
+    ]),
+    html.Div(className="div3", children=[
+        html.Div("div3")
+    ]),
+    html.Div(className="div4", children=[
+        dcc.Graph(className="graph", figure=fig)
+    ]),
+    html.Div(className="div5", children=[
+        html.Div("div5")
+    ]),
+    html.Div(className="div6", children=[
+        html.Div("div6")
+    ]),
+    html.Div(className="div7", children=[
+        html.Div("div7")
+    ])
 ])
 
 if __name__ == '__main__':
-    app.run_server(port=9001, debug=True)
+    app.run_server(debug=True)
