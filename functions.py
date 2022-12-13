0d7a255a6c82287296069a6c495df9440f3b2990
Anze <novak.anze1@gmail.com>
2022-12-13 20:06:35 +0100

    Dejavnosti version 2

--- functions.py
+++ functions.py
@@ -10,44 +10,38 @@ def skupna_poraba_obcine(record):
                 print("neki")
             poraba += int(value)
     return poraba
+def izrisi_dejavnosti_obcine():
+    data = list(DictReader(open('data/podatki_po_dejavnostih.csv', 'rt', encoding='windows-1250')))
+    obcine = set()
+    dejavnosti = set()
+    for record in data:
+        #if record['OBČINE'] != 'Slovenija' and record['OBČINE'] != 'Neznano':
 
-data = list(DictReader(open('data/podatki_po_dejavnostih.csv', 'rt', encoding='windows-1250')))
-obcine = set()
-dejavnosti = set()
-for record in data:
-    #if record['OBČINE'] != 'Slovenija' and record['OBČINE'] != 'Neznano':
+        if "/" in record["OBČINE"]:
+            obcine.add(record['OBČINE'].split("/")[0])
+        else:
+            obcine.add(record['OBČINE'])
+        dejavnosti.add(record['SKD DEJAVNOST'])
 
-    if "/" in record["OBČINE"]:
-        obcine.add(record['OBČINE'].split("/")[0])
-    else:
-        obcine.add(record['OBČINE'])
-    dejavnosti.add(record['SKD DEJAVNOST'])
+    dejavnosti_obcine = dict()
+    # 24 polj zacne se z januar 2020 konca z december 2021
+    poraba_po_mesecih = []
+    obcine = sorted(list(obcine))
+    dejavnosti = sorted(list(dejavnosti))
 
-dejavnosti_obcine = dict()
-# 24 polj zacne se z januar 2020 konca z december 2021
-poraba_po_mesecih = []
-obcine = sorted(list(obcine))
-dejavnosti = sorted(list(dejavnosti))
+    dejavnosti_obcine["OBČINE"] = obcine
+    for dejavnost in dejavnosti:
+        poraba_po_obcinah = []
+        for obcina in obcine:
+            dejavnost_je_v_obcini = False
+            for record in data:
+                if record['OBČINE'] == obcina and record['SKD DEJAVNOST'] == dejavnost:
+                    dejavnost_je_v_obcini = True
+                    poraba_po_obcinah.append(skupna_poraba_obcine(record))
+            if not dejavnost_je_v_obcini:
+                poraba_po_obcinah.append(0)
+        dejavnosti_obcine[dejavnost] = pd.DataFrame(list(zip(obcine, poraba_po_obcinah)), columns =['Občina', 'Poraba'])
+        poraba_po_obcinah = []
 
-dejavnosti_obcine["OBČINE"] = obcine
-for dejavnost in dejavnosti:
-    poraba_po_obcinah = []
-    for obcina in obcine:
-        dejavnost_je_v_obcini = False
-        for record in data:
-            if record['OBČINE'] == obcina and record['SKD DEJAVNOST'] == dejavnost:
-                dejavnost_je_v_obcini = True
-                poraba_po_obcinah.append(skupna_poraba_obcine(record))
-        if not dejavnost_je_v_obcini:
-            poraba_po_obcinah.append(0)
-    dejavnosti_obcine[dejavnost] = pd.DataFrame(list(zip(obcine, poraba_po_obcinah)), columns =['Občina', 'Poraba'])
-    poraba_po_obcinah = []
-
-    print(dejavnost)
-    print("---------------------")
-
-    print(dejavnosti_obcine[dejavnost])
-    print("---------------------")
-    print()
-    print()
+    return dejavnosti_obcine
 
