4aa91db55615982131e22cef1da0cb106a344357
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-20 10:29:56 +0100

    anze merge

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 df_prebivalci = pd.read_csv("data/podatki_starost_prebivalcev.csv", delimiter=",", encoding="windows-1250")
@@ -130,3 +131,50 @@ def izracun_normalizacije_poslovni_objekti(mapdf, leto):
 
     df = df.drop(columns={0})
     return df
+
+from csv import DictReader
+import pandas as pd
+
+
+def skupna_poraba_obcine(record):
+    poraba = 0
+    for key, value in record.items():
+        if key != 'OBČINE' and key != 'SKD DEJAVNOST' and value != 'z' and value != 'N':
+            if value == 'N':
+                print("neki")
+            poraba += int(value)
+    return poraba
+def izrisi_dejavnosti_obcine():
+    data = list(DictReader(open('data/podatki_po_dejavnostih.csv', 'rt', encoding='windows-1250')))
+    obcine = set()
+    dejavnosti = set()
+    for record in data:
+        #if record['OBČINE'] != 'Slovenija' and record['OBČINE'] != 'Neznano':
+
+        if "/" in record["OBČINE"]:
+            obcine.add(record['OBČINE'].split("/")[0])
+        else:
+            obcine.add(record['OBČINE'])
+        dejavnosti.add(record['SKD DEJAVNOST'][2:])
+
+    dejavnosti_obcine = dict()
+    # 24 polj zacne se z januar 2020 konca z december 2021
+    poraba_po_mesecih = []
+    obcine = sorted(list(obcine))
+    dejavnosti = sorted(list(dejavnosti))
+
+    dejavnosti_obcine["OBČINE"] = obcine
+    for dejavnost in dejavnosti:
+        poraba_po_obcinah = []
+        for obcina in obcine:
+            dejavnost_je_v_obcini = False
+            for record in data:
+                if record['OBČINE'] == obcina and record['SKD DEJAVNOST'] == dejavnost:
+                    dejavnost_je_v_obcini = True
+                    poraba_po_obcinah.append(skupna_poraba_obcine(record))
+            if not dejavnost_je_v_obcini:
+                poraba_po_obcinah.append(0)
+        dejavnosti_obcine[dejavnost] = pd.DataFrame(list(zip(obcine, poraba_po_obcinah)), columns =['Občina', 'Poraba'])
+        poraba_po_obcinah = []
+
+    return dejavnosti_obcine
